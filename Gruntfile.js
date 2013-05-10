98b7c707b1194176cf0bfc4ea9793f4b657d67a2
Derrick Weis <derrick@derrickweis.com>
2013-05-09 22:51:55 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '.'
+        }
+      }
+    },
+
+
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: "src",
+          dir: "build",
+
+          optimize: 'uglify2',
+          optimizeCss: 'standard',
+          inlineText: true,
+
+          modules: [
+            { name: 'cannon_app' }
+          ]
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      source: [ 'src/**/*.js' ]
+    },
+
+    regarde: {
+      js: {
+        files: [ 'src/**/*.js' ],
+        tasks: [ 'jshint', 'livereload' ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', [ 'jshint', 'requirejs' ]);
+  grunt.registerTask('watch',  [ 'livereload-start', 'regarde' ]);
+  grunt.registerTask('server', [ 'livereload-start', 'connect', 'regarde' ]);
+};
